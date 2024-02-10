205ebe1d125598b9790d854cf0d45d5e5b72b952
Dan <iam@dancielos.com>
2024-02-10 08:41:45 -0700

    updated the wireframe

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -18,34 +18,33 @@ const images = [
 export default function ImageSlider() {
 	return (
 		<div className='p-4'>
-			<div className='container relative mb-2 h-[360px]'>
-				<div className='container absolute h-full w-11/12 z-10 mx-auto left-1/2 transform -translate-x-1/2'>
-					<Image
-						src={images[0]}
-						alt='dog image'
-						fill
-						objectFit='cover'
-						className='rounded-[1px]'
-					/>
-					<div className='flex justify-between p-3'>
-						<span className='relative z-30 py-1 px-2 rounded-[1px] bg-slate-950/50 text-white text-sm'>
-							1 / 6
-						</span>
-						<button className='relative z-30 bg-slate-950/50 px-2 py-1 rounded-[1px]'>
-							<Image
-								src={fullScreenIcon}
-								alt='enter full screen'
-								className='w-4 fill-white'
-								width={24}
-								height={24}
-							/>
-						</button>
-					</div>
+			<div className='container relative mb-2 h-[360px] overflow-hidden'>
+				<Image
+					src={images[0]}
+					alt='dog image'
+					fill
+					objectFit='cover'
+					className='rounded-[1px]'
+				/>
+				<div className='flex justify-between p-3 items-start'>
+					<span className='relative z-30 py-1 px-2 rounded-[1px] bg-slate-950/50 text-white text-xs'>
+						1 / 6
+					</span>
+					<button className='relative z-30 bg-slate-950/50 p-2 rounded-[1px]'>
+						<Image
+							src={fullScreenIcon}
+							alt='enter full screen'
+							className='w-4 fill-white'
+							width={24}
+							height={24}
+						/>
+					</button>
 				</div>
+				{/* <div className='container absolute h-full w-11/12 z-10 mx-auto left-1/2 transform -translate-x-1/2'></div> */}
 
-				<div className='absolute top-1/2 left-0 transform -translate-y-1/2 w-full z-20'>
-					<div className='flex justify-between'>
-						<button className='bg-gray-100 p-1 py-2 shadow-sm rounded-[1px]'>
+				<div className='absolute top-1/2 left-0 transform -translate-y-1/2 w-full z-20 h-full'>
+					<div className='flex justify-between h-full'>
+						<button className='px-2 hover:bg-slate-800/30'>
 							<Image
 								src={leftIcon}
 								alt='image slider left nav'
@@ -54,7 +53,7 @@ export default function ImageSlider() {
 								height={50}
 							/>
 						</button>
-						<button className='bg-gray-100 p-1 py-2 shadow-sm rounded-[1px]'>
+						<button className='px-2 hover:bg-slate-800/30'>
 							<Image
 								src={rightIcon}
 								alt='image slider right nav'
